5b9313716c12f682ad202cd96fa0cf28b4e0ef5f
Lukasz Malucha <lucasmalucha@gmail.com>
2017-09-12 13:17:53 +0100

    Add files via upload

---
+++ data_preprocessing_template.R
@@ -0,0 +1,17 @@
+# Data Preprocessing Template with R
+
+# I. Import dataset
+dataset = read.csv('Data.csv')
+
+
+# install.packages('caTools')
+library(caTools)
+# Splitting the dataset into the Training set and Test set
+set.seed(123)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8) 
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
