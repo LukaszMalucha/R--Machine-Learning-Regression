3cedb3777ab955b23e0f5fb70316440314db67c2
Lukasz Malucha <lucasmalucha@gmail.com>
2017-09-13 12:26:59 +0100

    Update data_preprocessing_template.R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -15,3 +15,24 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+                              ###### OPTIONAL ######
+
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()                             ## Categorical data encoded into number
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])             
+onehotencoder = OneHotEncoder(categorical_features = [0])   ## Encode countries into 1-0-0 to not affect equation.
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
