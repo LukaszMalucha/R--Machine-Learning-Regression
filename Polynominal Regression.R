d58f262cb1d290a1fb09f24d60514c0c9e2e4a12
Lukasz Malucha <lucasmalucha@gmail.com>
2017-09-14 16:33:39 +0100

    Add files via upload

---
+++ Polynominal Regression.R
@@ -0,0 +1,74 @@
+# Polynomial Regression
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+
+## Position column is redundant to level column, so let's remove it:
+dataset = dataset[2:3]
+
+
+# Fitting Linear Regression to the dataset
+lin_reg = lm(formula = Salary ~ .,
+             data = dataset)
+
+# Fitting Polynomial Regression to the dataset
+## Polynomial features - extra 'power" levels by creating additional columns:
+dataset$Level2 = dataset$Level^2           ## Additional column for ^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+
+## Summary shows that all added columns have statistically significant p-value
+
+## Polynominal Regression created with lm function: 
+poly_reg = lm(formula = Salary ~ .,     
+              data = dataset)
+
+# Visualising the Linear Regression results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Reality Check (Linear Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the Polynomial Regression results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)), ## Swap lin_reg with poly_reg
+            colour = 'blue') +
+  ggtitle('Reality Check (Polynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the Regression Model results (for higher resolution and smoother curve)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(poly_reg,
+                                        newdata = data.frame(Level = x_grid,
+                                                             Level2 = x_grid^2,
+                                                             Level3 = x_grid^3,
+                                                             Level4 = x_grid^4))),
+            colour = 'blue') +
+  ggtitle('Reality Check (Polynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
